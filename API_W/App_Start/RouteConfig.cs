989ad9988c2fece93086db73a92a95145d8b5f1c
mitschi11051994 <mitschi1994@hotmail.com>
2017-08-21 00:01:37 -0600

    ADD VIEWS

--- API_W/App_Start/RouteConfig.cs
+++ API_W/App_Start/RouteConfig.cs
@@ -13,11 +13,7 @@ namespace API_W
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            
         }
     }
 }
