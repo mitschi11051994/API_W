9ba9e2c00c93049e8381d8a57a7f76571c1d8657
mitschi11051994 <mitschi1994@hotmail.com>
2017-08-20 23:00:12 -0600

    ADD Views

--- API_W.Tests/Controllers/HomeControllerTest.cs
+++ API_W.Tests/Controllers/HomeControllerTest.cs
@@ -1,25 +1,7 @@
-﻿using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using API_W;
-using API_W.Controllers;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace API_W.Tests.Controllers
 {
-    [TestClass]
-    public class HomeControllerTest
-    {
-        [TestMethod]
-        public void Index()
-        {
-            // Disponer
-            HomeController controller = new HomeController();
 
-            // Actuar
-            ViewResult result = controller.Index() as ViewResult;
-
-            // Declarar
-            Assert.IsNotNull(result);
-            Assert.AreEqual("Home Page", result.ViewBag.Title);
-        }
-    }
+    
 }
